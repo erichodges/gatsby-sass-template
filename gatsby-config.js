1e042dad18343f637ee302bdd27c67251ad250a8
Eric Hodges <ehodges2004@gmail.com>
2018-04-17 12:27:14 -0700

    deleted typography from plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,6 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
 
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },
-
 
   ]
 }
