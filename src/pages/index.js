1b1ae38f9acd22619b72c9e0ecfa2e4af18df24a
Eric Hodges <ehodges2004@gmail.com>
2018-04-17 14:36:32 -0700

    added google font to sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,9 @@ import { locationsAreEqual } from 'history';
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    
+    <b>Welcome to your new Gatsby site.</b>
+    
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium
       rerum delectus est soluta ullam unde eius dolorum expedita sapiente,
