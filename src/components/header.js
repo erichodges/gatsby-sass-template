e534f93f90957d5d861c44ce8cfa0849a2bfb058
Eric Hodges <ehodges2004@gmail.com>
2018-04-23 14:11:05 -0700

    added basic hamburger placement

--- src/components/header.js
+++ src/components/header.js
@@ -4,22 +4,25 @@ import Link from 'gatsby-link'
 const Header = () => (
   <div>
     <div className="header">
-      <div>
-        <svg className="hamburger__icon">
-          <use xlinkHref="img/hamburger.svg#hamburger"></use>
-        </svg>
+      <div className="hamburger-logo">
+        <div className="hamburger-icon">
+          <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24"            xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none"/>
+            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+          </svg>
+        </div>
+        <h1 className="logo">
+          <Link
+            to="/" 
+            style={{
+              color: 'black',
+              textDecoration: 'none',
+            }}
+            >
+            Site Logo
+          </Link>
+        </h1>
       </div>
-      <h1 className="logo">
-        <Link
-          to="/" 
-          style={{
-            color: 'black',
-            textDecoration: 'none',
-          }}
-          >
-          Site Logo
-        </Link>
-      </h1>
       <div className="header-links">          
         <Link to="page-2" className="header-page-link">
           <div className="header-links__page">Page 2</div>
