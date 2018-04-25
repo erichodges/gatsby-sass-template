e6be32481bd8b94c9aa3a0610361a2f8c63d349a
Eric Hodges <ehodges2004@gmail.com>
2018-04-25 08:47:04 -0700

    working on bg image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,23 @@ import Link from 'gatsby-link'
 import { locationsAreEqual } from 'history';
 
 const IndexPage = () => (
-  <div>    
-    <h2 className="h1">Main Heading</h2>
-    
-      <h3>Welcome to your new Gatsby site.</h3>
+  <container className="container">
+    <div>    
+      <h2 className="h1">Main Heading</h2>
+      
+        <h3>Welcome to your new Gatsby site.</h3>
 
-    <p>      
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium
-      rerum delectus est soluta ullam unde eius dolorum expedita sapiente,
-      eveniet, nulla harum? Quidem, molestias nisi! Est, ad at! Optio,
-      doloremque. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-      Accusantium rerum delectus est soluta ullam unde eius dolorum expedita
-      sapiente, eveniet, nulla harum? Quidem, molestias nisi! Est, ad at! Optio,
-      doloremque.
-    </p>    
-  </div>
+      <p>      
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium
+        rerum delectus est soluta ullam unde eius dolorum expedita sapiente,
+        eveniet, nulla harum? Quidem, molestias nisi! Est, ad at! Optio,
+        doloremque. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Accusantium rerum delectus est soluta ullam unde eius dolorum expedita
+        sapiente, eveniet, nulla harum? Quidem, molestias nisi! Est, ad at! Optio,
+        doloremque.
+      </p>    
+    </div>
+  </container>
 )
 
 export default IndexPage
